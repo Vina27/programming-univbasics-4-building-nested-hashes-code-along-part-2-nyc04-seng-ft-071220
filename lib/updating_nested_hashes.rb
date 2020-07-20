c13b072fe7eff5a41952f15d82dbdc1dc36d804c
Maysoon Tabali <mtab03@me.com>
2020-07-20 13:49:34 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -22,7 +22,7 @@ def update_status
 
 #update_status 
 
-epic_tragedy[:hero][:status] = "dead" 
+update_status = epic_tragedy[:hero][:status] = "dead" 
 epic_tragedy[:heroine][:status] = "dead"
  end
  epic_tragedy 
