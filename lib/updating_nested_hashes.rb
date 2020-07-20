a6e979e327964c319142bf6fe4287c228bd9dfca
Maysoon Tabali <mtab03@me.com>
2020-07-20 14:22:02 +0000

    Automatically backed up by Learn

--- lib/updating_nested_hashes.rb
+++ lib/updating_nested_hashes.rb
@@ -77,7 +77,7 @@ end
 #:additional_characters => [{name: "Prince Escalus"}, {name:  
  # "Apothecary"}] 
 #epic_tragedy.push[:additional_characters]
-  epic_tragedy[:additional_characters => [{name: "Prince Escalus"}, {name: "Apothecary"}]
+  #epic_tragedy[:additional_characters => [{name: "Prince Escalus"}, {name: "Apothecary"}]
   
 epic_tragedy[:additional_characters][{name: "Prince Escalus"}, {name: "Apothecary"}]
 #hash_name[key][key within key]=value
